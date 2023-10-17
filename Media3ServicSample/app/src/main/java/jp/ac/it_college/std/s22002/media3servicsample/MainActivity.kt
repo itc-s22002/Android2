dfc4ab577a912bed5e7526ce2b78fa71efa598f5
s22002 <s22002@std.it-college.ac.jp>
2023-10-17 11:13:29 +0900

    Media3を使ったサンプル

---
+++ Media3ServicSample/app/src/main/java/jp/ac/it_college/std/s22002/media3servicsample/MainActivity.kt
@@ -0,0 +1,56 @@
+package jp.ac.it_college.std.s22002.media3servicsample
+
+import android.content.ComponentName
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.media3.common.MediaItem
+import androidx.media3.session.MediaController
+import androidx.media3.session.SessionToken
+import com.google.common.util.concurrent.ListenableFuture
+import jp.ac.it_college.std.s22002.media3servicsample.databinding.ActivityMainBinding
+import java.util.concurrent.Future
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var controllerFuture: ListenableFuture<MediaController>
+
+    private val controller: MediaController?
+        get() = if (controllerFuture.isDone) controllerFuture.get() else null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.play.setOnClickListener{playSample()}
+        binding.pause.setOnClickListener{pauseSample()}
+    }
+
+    override fun onStart() {
+        super.onStart()
+        controllerFuture = MediaController.Builder(
+            this,
+            SessionToken(this, ComponentName(this, MediaPlaybackService::class.java))
+        ).buildAsync()
+    }
+
+    override fun onStop() {
+        MediaController.releaseFuture(controllerFuture)
+        super.onStop()
+    }
+
+    private fun playSample(){
+        val controller = this.controller ?: return
+        controller.setMediaItem(
+            MediaItem.fromUri("android.resource//${packageName}/${R.raw.sample}")
+        )
+        controller.seekToDefaultPosition(0)
+        controller.play()
+    }
+
+    private fun pauseSample(){
+        val controller =this.controller ?: return
+        controller.stop()
+
+    }
+}
