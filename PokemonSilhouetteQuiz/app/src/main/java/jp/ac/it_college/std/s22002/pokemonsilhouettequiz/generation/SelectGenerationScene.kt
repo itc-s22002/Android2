0218304ceb82362a50b26db561803b7bc4104b91
s22002 <s22002@std.it-college.ac.jp>
2023-11-14 10:54:27 +0900

    世代選択画面

---
+++ PokemonSilhouetteQuiz/app/src/main/java/jp/ac/it_college/std/s22002/pokemonsilhouettequiz/generation/SelectGenerationScene.kt
@@ -0,0 +1,68 @@
+package jp.ac.it_college.std.s22002.pokemonsilhouettequiz.generation
+
+import android.content.ClipData.Item
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import jp.ac.it_college.std.s22002.pokemonsilhouettequiz.R
+import jp.ac.it_college.std.s22002.pokemonsilhouettequiz.ui.theme.PokemonSilhouetteQuizTheme
+
+@Composable
+fun SelectGenerationScene(modifier: Modifier = Modifier){
+    Surface (modifier) {
+                LazyColumn(modifier = Modifier.fillMaxSize()){
+                    items(1){
+                        ItemGeneration(generation = 9, seriesName = "スカーレット\nバイオレット")
+                    }
+                }
+    }
+}
+
+@Composable
+fun ItemGeneration(generation: Int, seriesName:String){
+    Surface (
+        color = MaterialTheme.colorScheme.tertiary,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+    ){
+        Column (
+            modifier = Modifier
+                .padding(8.dp)
+        ){
+            Text(
+                text = stringResource(id = R.string.generation, generation),
+                style = MaterialTheme.typography.titleLarge
+            )
+            Text(
+                text = seriesName,
+                style = MaterialTheme.typography.titleMedium
+            )
+
+        }
+
+    }
+
+
+
+}
+
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+fun SelectGenerationScenePreview(){
+    PokemonSilhouetteQuizTheme {
+        SelectGenerationScene(Modifier.fillMaxSize())
+    }
+}
