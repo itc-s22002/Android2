b09c4a531dbd23c91e6483deb0bc7b50b0c78814
s22002 <s22002@std.it-college.ac.jp>
2023-11-28 12:19:38 +0900

    titleと選択画面３つまで

--- WeatherMap/app/build.gradle.kts
+++ WeatherMap/app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
     kotlin("plugin.serialization")
 }
 
@@ -42,7 +41,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
+        kotlinCompilerExtensionVersion = "1.5.2"
     }
     packaging {
         resources {
@@ -53,7 +52,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.1")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
@@ -73,6 +72,9 @@ dependencies {
     implementation("io.ktor:ktor-client-cio:2.3.6")
     implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.6")
     implementation("io.ktor:ktor-client-content-negotiation:2.3.6")
+
+    implementation("io.coil-kt:coil-compose:2.5.0")
+
 
 
 
---
+++ WeatherMap/app/src/main/java/jp/ac/it_college/std/s22002/weathermap/city/SelectCityScene.kt
@@ -0,0 +1,77 @@
+package jp.ac.it_college.std.s22002.weathermap.city
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import jp.ac.it_college.std.s22002.weathermap.ui.theme.WeatherMapTheme
+
+@Composable
+fun SelectCityScene(modifier: Modifier = Modifier, onCitySelected: (Int) -> Unit = {}){
+    Surface (modifier){
+        LazyColumn(modifier = Modifier.fillMaxWidth()){
+            item(1) {
+                City(
+                    cityId = 2130037,
+                    cityName = "北海道(札幌)",
+                    onCitySelected = onCitySelected
+                )
+            }
+            item(2){
+                City(
+                    cityId = 2130658,
+                    cityName = "青森",
+                    onCitySelected = onCitySelected
+                )
+            }
+            item(3){
+                City(
+                    cityId = 2111834,
+                    cityName = "岩手(盛岡)",
+                    onCitySelected = onCitySelected
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun City(cityId: Int, cityName: String, onCitySelected: (Int) -> Unit = {}) {
+    // 背景色・文字色を全体的に設定するために使ってる
+    Surface(
+        color = MaterialTheme.colorScheme.tertiary,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .clickable { onCitySelected(cityId) }
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(8.dp)
+
+        ) {
+            // 第?世代
+            Text(
+                text = cityName,
+                style = MaterialTheme.typography.titleLarge
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+fun SelectGenerationScenePreview() {
+    WeatherMapTheme {
+        SelectCityScene(Modifier.fillMaxSize())
+    }
+}
