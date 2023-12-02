e196cf402121148236bdd8b70dac0de7a997013c
s22002 <s22002@std.it-college.ac.jp>
2023-12-02 13:50:16 +0900

    エラー解決

--- WeatherMap/app/src/main/java/jp/ac/it_college/std/s22002/weathermap/api/CityGroup.kt
+++ WeatherMap/app/src/main/java/jp/ac/it_college/std/s22002/weathermap/api/CityGroup.kt
@@ -1,6 +1,5 @@
 package jp.ac.it_college.std.s22002.weathermap.api
 
-import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import jp.ac.it_college.std.s22002.weathermap.api.model.WeatherList
 object CityGroup {
